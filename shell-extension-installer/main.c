7e3fdfa59a294e39b6c1a4af7d849dcce30f0df9
KHOPAN <puthipnakchan@gmail.com>
2024-06-23 22:36:13 +0700

    Add shell notifier

--- shell-extension-installer/main.c
+++ shell-extension-installer/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <khopanwin32.h>
 #include <khopanstring.h>
+#include <ShlObj_core.h>
 
 // {E761549A-5209-4183-88CA-D587FCD89393}
 EXTERN_GUID(CLSID_SIMPLE_SHELL_EXTENSION, 0xE761549A, 0x5209, 0x4183, 0x88, 0xCA, 0xD5, 0x87, 0xFC, 0xD8, 0x93, 0x93);
@@ -49,6 +50,7 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, NULL, NULL);
 	printf("Installation finished\n");
 	return 0;
 }
