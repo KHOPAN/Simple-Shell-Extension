fd47e36f04fa74fc1c5cf729fa2f7070b3ee69ee
KHOPAN <puthipnakchan@gmail.com>
2024-06-23 22:22:47 +0700

    Add log statement

--- shell-extension/main.c
+++ shell-extension/main.c
@@ -1,10 +1,13 @@
 #include <Windows.h>
 
 BOOL WINAPI DllMain(HINSTANCE instance, DWORD flags, LPVOID reserved) {
+	MessageBoxW(NULL, L"DllMain()", L"Simple Shell Extension", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
 	return TRUE;
 }
 
 _Check_return_ STDAPI DllGetClassObject(_In_ REFCLSID classIdentifier, _In_ REFIID identifier, _Outptr_ LPVOID FAR* output) {
+	MessageBoxW(NULL, L"DllGetClassObject()", L"Simple Shell Extension", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+
 	if(!output) {
 		return E_INVALIDARG;
 	}
@@ -14,5 +17,6 @@ _Check_return_ STDAPI DllGetClassObject(_In_ REFCLSID classIdentifier, _In_ REFI
 }
 
 __control_entrypoint(DllExport) STDAPI DllCanUnloadNow(void) {
+	MessageBoxW(NULL, L"DllCanUnloadNow()", L"Simple Shell Extension", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
 	return S_OK;
 }
